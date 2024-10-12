903e9e3ca7d86f8f3f806089fe37a49aa5b0e88a
gxb <gxbshiny@163.com>
2024-10-12 18:04:21 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
   <router-view />
+  <HelloWorld msg="1123"></HelloWorld>
 </template>
 
 <style scoped></style>
